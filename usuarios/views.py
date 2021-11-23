d81c4aed9691ab8922e617de887fc36290960899
Marcos Santos <marcos.diego.ns@outlook.com>
2021-11-23 13:50:00 -0500

    create cadastro form view

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+        print('Usuário cadastrado com sucesso!')
+        return redirect(reverse('login'))
+    else:
+        return render(request, 'usuarios/cadastro.html')
 
 
 def login(request):
