977d9bba698ca3ff4b2954ea7d777eece5895059
Marcos Santos <marcos.diego.ns@outlook.com>
2021-11-23 08:33:51 -0500

    add pessoas model and register admin

--- pessoas/admin.py
+++ pessoas/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Pessoa
+
+
+class ListandoPessoas(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_per_page = 10
+
+
+admin.site.register(Pessoa, ListandoPessoas)
