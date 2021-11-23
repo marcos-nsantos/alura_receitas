28c73791352ebfc2a742a42309ed761e8a5b5ab2
Marcos Santos <marcos.diego.ns@outlook.com>
2021-11-23 14:14:04 -0500

    validate data and create user

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.models import User
 
 
 def cadastro(request):
@@ -7,6 +8,21 @@ def cadastro(request):
         email = request.POST['email']
         senha = request.POST['password']
         senha2 = request.POST['password2']
+        if not nome.strip():
+            print('Nome não pode ser vazio')
+            return redirect(reverse('cadastro'))
+        if not email.strip():
+            print('Email não pode ser vazio')
+            return redirect(reverse('cadastro'))
+        if senha2 != senha:
+            print('Senhas não conferem')
+            return redirect(reverse('cadastro'))
+        if User.objects.filter(email=email).exists():
+            print('Email já cadastrado')
+            return redirect(reverse('cadastro'))
+
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
         print('Usuário cadastrado com sucesso!')
         return redirect(reverse('login'))
     else:
