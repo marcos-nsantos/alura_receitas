b247b8f09374e88552a7aec660a0bba69d4f6343
Marcos Santos <marcos.diego.ns@outlook.com>
2021-11-23 09:22:29 -0500

    add image field

--- alura_receita/settings.py
+++ alura_receita/settings.py
@@ -126,6 +126,10 @@ STATIC_ROOT = BASE_DIR / 'receitas/static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'alura_receita/static']
 
+# Media files (images, videos, etc)
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
